20e9a6cf717a0a19095e87bec6331a357ab179fe
Mahbub Alam <60845341+smmahbub97@users.noreply.github.com>
2020-02-27 11:49:03 +0600

    Add files via upload
    commit 1

---
+++ Lab Final/models/user-model.js
@@ -0,0 +1,45 @@
+var db = require('./db');
+
+module.exports ={
+	getById: function(id, callback){
+		var sql = "select * from utype where id=?";
+		db.getResult(sql, [id], function(result){
+			if(result.length > 0){
+				callback(result[0]);
+			}else{
+				callback(null);
+			}
+		});
+	},
+	getByUname: function(uname, callback){
+		var sql = "select * from user where username=?";
+		db.getResult(sql, [uname], function(result){
+			if(result.length > 0){
+				callback(result[0]);
+			}else{
+				callback(null);
+			}
+		});
+	},
+	validate: function(user, callback){
+		var sql = "select * from user where username=? and password=?";
+		db.getResult(sql, [user.username, user.password], function(result){
+			if(result.length > 0){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+	getAll:function(callback){
+		var sql = "select * from user";
+		db.getResult(sql, null, function(results){
+			if(results.length > 0){
+				callback(results);
+			}else{
+				callback(null);
+			}
+		});
+	},
+	
+}
